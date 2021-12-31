bf505d52758c71665808f60b62832db36f3c6100
EduardoCL <35414231+EduardoCortesL@users.noreply.github.com>
2021-12-31 14:41:47 -0800

    navbar & start of homepage working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 import Layout, { Content } from "antd/lib/layout/layout";
@@ -12,11 +11,9 @@ const Home = () => {
                 <div className="row gx-5 align-items-center">
                     <div className="col-lg-6">
                         <div className="mb-5 mb-lg-0 text-center text-lg-start">
-                            <h1 className="display-1 lh-1 mb-3">Showcase your app beautifully.</h1>
-                            <p className="lead fw-normal text-muted mb-5">Launch your mobile app landing page faster with this free, open source theme from Start Bootstrap!</p>
+                            <h1 className="display-1 lh-1 mb-3">Welcome to EventMe.</h1>
+                            <p className="lead fw-normal text-muted mb-5">Easy. Simple. Effective.</p>
                             <div className="d-flex flex-column flex-lg-row align-items-center">
-                                <a className="me-lg-3 mb-4 mb-lg-0" href="#!"><img className="app-badge" src="assets/img/google-play-badge.svg" alt="..." /></a>
-                                <a href="#!"><img className="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
                             </div>
                         </div>
                     </div>
@@ -29,17 +26,7 @@ const Home = () => {
                                         <stop className="gradient-end-color" offset="100%"></stop>
                                     </linearGradient>
                                 </defs>
-                                <circle cx="50" cy="50" r="50"></circle></svg
-                            ><svg className="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
-                                <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect></svg
-                            ><svg className="shape-2 d-none d-sm-block" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50"></circle></svg>
-                            <div className="device-wrapper">
-                                <div className="device" data-device="iPhoneX" data-orientation="portrait" data-color="black">
-                                    <div className="screen bg-black">
-                                    </div>
-                                </div>
-                            </div>
+                                <circle cx="50" cy="50" r="50"></circle></svg>
                         </div>
                     </div>
                 </div>
