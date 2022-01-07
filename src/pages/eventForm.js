f513be8d2faa0a6e99d5f8722eea665deeb995ad
EduardoCL <35414231+EduardoCortesL@users.noreply.github.com>
2022-01-06 19:37:22 -0800

    create event working, pushing data to DB, needs finishing touches

--- src/pages/eventForm.js
+++ src/pages/eventForm.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { Row, Col, Form, DatePicker, Input, Layout } from "antd";
 import "antd/dist/antd.min.css";
-
+import axios from "axios";
+import { date } from "yup";
 
 const { Content } = Layout;
 
@@ -10,7 +11,173 @@ const layout = {
   wrapperCol: { span: 6 },
 };
 
+export default class EventForm extends Component {
+  constructor(props) {
+    super(props);
 
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onChangeEnd = this.onChangeEnd.bind(this);
+    this.onChangeLocation = this.onChangeLocation.bind(this);
+
+    this.state = {
+      name: "",
+      date: new Date(),
+      end: new Date(),
+      location: "",
+      guest: [],
+    };
+  }
+
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
+  onChangeEnd(date) {
+    this.setState({
+      end: date,
+    });
+  }
+
+  onChangeLocation(e) {
+    this.setState({
+      location: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    const event = {
+      name: this.state.name,
+      date: this.state.date,
+      end: this.state.end,
+      location: this.state.location,
+      guest: this.state.guest,
+    };
+
+    console.log(event);
+
+    axios
+      .post("http://localhost:4000/events/add", event)
+      .then((res) => console.log(res.data));
+  }
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Content>
+            <h1 className="display-3"> Please Fill Up The Form Below</h1>
+            <Form {...layout} onFinish={this.onSubmit}>
+              <Row>
+                <Col span={8} offset={8}>
+                  <Form.Item
+                    label="Event Name"
+                    name="eventName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the name of the Event",
+                      },
+                    ]}
+                  >
+                    <Input
+                      value={this.state.name}
+                      onChange={this.onChangeName}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={8} offset={8}>
+                  <Form.Item
+                    label="Event Date"
+                    name="eventDate"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the start date of the Event",
+                      },
+                    ]}
+                  >
+                    <DatePicker
+                      showTime
+                      format="YYYY-MM-DD HH:mm"
+                      value={this.state.date}
+                      onChange={this.onChangeDate}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={8} offset={8}>
+                  <Form.Item
+                    label="Event End"
+                    name="eventEnd"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the end date of the Event",
+                      },
+                    ]}
+                  >
+                    <DatePicker
+                      showTime
+                      format="YYYY-MM-DD HH:mm"
+                      value={this.state.end}
+                      onChange={this.onChangeEnd}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={8} offset={8}>
+                  <Form.Item
+                    label="Event Location"
+                    name="eventLocation"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the name of the Event location",
+                      },
+                    ]}
+                  >
+                    <Input
+                      value={this.state.location}
+                      onChange={this.onChangeLocation}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 8,
+                }}
+              >
+                <button
+                  className="btn btn-primary btn-lg rounded-pill"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </Form.Item>
+            </Form>
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+/*
 const EventForm = () => {
 
   const onFinish = (values) => {
@@ -111,3 +278,5 @@ const EventForm = () => {
 };
 
 export default EventForm;
+
+*/
